c600e99e7ce3f4ed0185e46c666601d72c95a327
Jason Wolfe <jason.wolfe71@gmail.com>
2020-11-02 15:21:53 -0600

    Comment and clean up the function

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,16 @@
-const cio = require('cheerio');
+/*
+project: cta-cloudfunctions
+file: index.js
+description: cloud functions required for the cta project for CS 372
 
+Author: Jason Wolfe (200377485)
+*/
+
+//node libraries
+const cio = require('cheerio');
 const fetch = require('node-fetch')
 
+//firebase libraries
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
@@ -11,30 +20,34 @@ const BASE_URL = 'https://dashboard.saskatchewan.ca';
 
 const db = admin.firestore();
 
-//functions.https.onRequest(async (req, res) =>
+
 exports.fetchCaseStatistics = functions.pubsub.schedule('every 1 hours from 13:00 to 15:00').onRun(async (context) => {
 
     const newCases = await getNewCases();
 
-    getNewCases(newCases);
+    getMaxCases(newCases);
 
     return 'success';
-    //return res.send(newCases);
 });
 
 exports.testFn = functions.https.onRequest(async (req, res) => {
     const newCases = await getNewCases();
+    //const newCases = JSON.parse(DUMMY_DATA);
+    getMaxCases(newCases);
 
-    //console.log(newCases);
-    await getMaxCases(newCases);
-
-    generateMaxCases();
-    res.send('yes');
+    
+    res.send(newCases);
 });
 
+/*
+getRegionID(region)
 
+    region: region in string form
+    return: the corresponding region ID
 
-//TODO: some how display and error if -1 is returned (for our purposes)
+    Description: used to transform the string representation of strings (as given in the govt data) to an integer
+                 id. Since it is much easier to deal with ints compared to string.
+*/
 function getRegionID(region) {
     let result;
     switch (region) {
@@ -79,37 +92,51 @@ function getRegionID(region) {
             break;
         default:
             result = -1;
+            console.log("WARN: regionID: " + region);
             break;
     }
     return result;
 }
 
+/*
+getNewCases()
+    return: an array of case objects corresponding to the new cases that were pushed to the cases collection
+
+    Description: the function scrapes the govt of sask covid information website to find the URL to the JSON data
+                 then the function retrieves this JSON data. With the data the function transforms it into a JS object
+                 to submit to firebase. In order to reduce the amount of data sent to firestore, the function fetches the
+                 date of the last case in firestore. It uses this date to filter out the new cases. At this point the
+                 function submits the new cases into firestore using a batching function.
+*/
 async function getNewCases() {
-    var _jsonURL;
-    var data;
+    var _jsonURL; //hold the url path to the JSON data of the case data
+    var data; //the json data fetched from the govt
     
+    //await a fetch for the JSON URL from the government website 
     await fetch(BASE_URL + '/health-wellness/covid-19/cases/')
     .then(res => res.text())
     .then(body => {
+        /* utilize the cheerio framework to scrape the website for the JSON url */
         let $ = cio.load(body);
         let jsonLink = $(".indicator-export > ul > li:contains('JSON')");
-        _jsonURL = BASE_URL + jsonLink.find('a').attr('href');
+        _jsonURL = BASE_URL + jsonLink.find('a').attr('href'); //combine the base URL (govt website) with the path to the JSON data
         
     })
     .catch(error => console.error(error));
 
+    //await a fetch for the JSON data from the govt website
     await fetch(_jsonURL)
     .then((response) => response.json())
     .then((json) => {
-        data = json;
+        data = json; //store the result in the data variable (so we can access through the function)
         
     })
     .catch((error) => console.error(error));
 
-
-
+    
+    //create a new map based on the data retrieved
     let transformed = data.map((item) => {
-        var temp = {};
+        var temp = {}; //create an empty object to store data
 
         //Convert the string date to a Date object
         var dateParts = item['Date'].split("/");
@@ -127,32 +154,33 @@ async function getNewCases() {
         temp.recoveredCases = item['Recovered Cases'];
         temp.deaths = item['Deaths'];
 
-        return temp;
+        return temp; //insert the resulting object into the 'transformed' array
     });
 
-    let casesRef = db.collection('cases');
-
-    let lastCase = await casesRef.orderBy('date', 'desc').limit(1).get();
+    //get the last case from firestore (based on date)
+    let lastCase = await db.collection('cases').orderBy('date', 'desc').limit(1).get();
     let prevDate;
 
     if(!lastCase.empty)
-        prevDate = lastCase.docs[0].data().date.toDate();
+        prevDate = lastCase.docs[0].data().date.toDate(); // set previous date to that which was retrieved from firestore
     else
-        prevDate = new Date(2020, 1, 1);
+        prevDate = new Date(2020, 1, 1); //else use an arbitrary date (pre covid in sask)
 
-    newCases = transformed.filter(element => element.date.getTime() > prevDate.getTime());
+    newCases = transformed.filter(element => element.date.getTime() > prevDate.getTime()); //filter the array to only the cases that have a date later than the last date in the firestore
 
     console.log("there are " + newCases.length  + " new cases");
 
+    //if there are less than 500 records that need to be uploaded (less than the firestore limit of 500 adds per second)
     if (newCases.length <= 500) {
-        let batch = db.batch();
+        let batch = db.batch(); //create a batch transaction
 
+        //for each object, create a document and set it in firestore and add to batch
         newCases.forEach(element => {
             let docRef = db.collection('cases').doc();
             batch.set(docRef, element);
         });
 
-        batch.commit();
+        batch.commit(); //commit the batch to firestore
     }
     else{
         //let totalRecords = newCases.length;
@@ -160,6 +188,10 @@ async function getNewCases() {
 
         let batch;
         
+        /* same premise as above, but we need to create separate batches (of max 500 records per second) to avoid hitting
+            transaction limits imposed by firestore 
+        */
+
         newCases.forEach(element => {
             if(index % 500 == 0){
                 if(index > 0){
@@ -176,74 +208,72 @@ async function getNewCases() {
             batch.set(docRef, element);
         });
 
+        //commit remaing cases
         if(index % 500 != 0) {
             console.log("Committing");
             batch.commit();
         }
     }
 
+    //return the new cases (for use later)
     return newCases;
 }
 
-async function getMaxCases(newCases) {
+
+/*
+getMaxCases(newCases)
+    newCases: an array of case objects corresponding to the new cases that were pushed to the cases collection
+
+    Description: the function finds the maximum amount of active cases for each region whenever it is called
+                 the new cases are passed to the function and then the function compares the new data to the 
+                 previously submitted data.
+*/
+function getMaxCases(newCases) {
+    //check to see if there are new cases.
+    if(newCases.length == 0) {
+        console.log("there are no new cases...not updating the max values");
+        //since there are no new cases, exit the function
+        return;
+    }
 
     //get new cases for today by region
     let newData = new Array();
-
+    //get the new active case numbers from the new batch of region information
     newCases.forEach((element) => {
-        newData[element.regionID] = element.newCases;
+        newData[element.regionID] = element.activeCases; //set the index (regionID) of newData to the active case numbers
     });
 
+    //fetch the current stored 'max-cases' information from Firestore
     db.collection('max-cases').get().then((snapshot) => {
-        let currentData = new Array()
+        let currentData = new Array(); //temporary array to store the retrieved data
+        //loop over each document retrieved in the snapshot and insert it into the temp array according to the ID
         snapshot.forEach((doc) => {
-            currentData[parseInt(doc.id)] = doc.data();
+            currentData[parseInt(doc.id)] = doc.data(); //doc.data is the object containing the region max-cases data
         });
-        console.log(currentData);
 
 
-        let regMax = 0;
-        let regToday = false;
+        let regMax = 0; //temp var to store the max cases
+        let regToday = false; //temp var to store if the max was achieved today
+        //loop over the 13 regions
         for(var i = 0; i < 13; i ++){
-            
-            if(newData[i] > currentData[i].max) {
-                regMax = newData[i];
-                regToday = true;
-            }
-            else {
-                regMax = currentData[i].max;
-                regToday = false;
-            }
 
+            if(newData[i] > currentData[i].max) { //check if the newData from the new cases is a max for the region
+                regMax = newData[i]; //set the max to the new data
+                regToday = true; //set today to true
+            }
+            else { //else it is not a max
+                regMax = currentData[i].max; //use the current max
+                regToday = false; //set today to false
+            }
+            //update the document to the new data
             db.collection('max-cases').doc(i.toString()).set({
                 max: regMax,
                 today: regToday
             });
         }
 
+    }).catch((error) => {
+        //there was an error
+        console.log("Unable to get current 'max-cases' data: ", error);
     });
-
-    //Get the current max values from firebase
-    //compare them to the new reports from today.
-    //filter out the ids who have new highs
-    //create a batch of updates
-    //set a bool of new high
-    //for those who did not have a new high, set bool to false.
-}
-
-
-function generateMaxCases() {
-    let batch = db.batch();
-
-    let template = {};
-
-    template.max = 0;
-    template.today = false;
-
-    for(var i = 0; i < 13; i ++) {
-        let docRef = db.collection('max-cases').doc(i.toString());
-        batch.set(docRef, template);
-    }
-
-    batch.commit(); 
 }
